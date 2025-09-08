07c328b1911da700d41288d6e71929a30c64d33e
Aytida-max <adi763638@gmail.com>
2025-09-08 20:57:02 +0530

    q

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions={
-    origin:["http://localhost:5173"
+    origin:["http://localhost:5173",
+        "https://job-portal-frontend-pi-amber.vercel.app"
     ],
     credentials: true,
 }
